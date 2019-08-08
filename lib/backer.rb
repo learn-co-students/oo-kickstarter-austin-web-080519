cfc9b12bda3e075e172d4a86b57ef96fde13b07b
hemeshvpatel <hemesh.v.patel@gmail.com>
2019-08-08 10:13:00 -0500

    Done.

---
+++ lib/backer.rb
@@ -0,0 +1,16 @@
+require_relative('project')
+
+class Backer
+   attr_reader :backed_projects, :name
+   
+   def initialize(name)
+       @name = name
+       @backed_projects = []
+   end
+   
+   def back_project(project)
+       @backed_projects << project
+       #project.add_backer(self) if !@backed_projects.include?(project)
+       project.backers << self 
+    end
+end
